be9b25574680b00de6c253905934b3c9c577984d
Luke_Z <zurawskiu@gmail.com>
2021-07-14 20:01:07 +0200

    Working at import svg files as react component.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import style from './app.module.css';
+import { ReactComponent as Test } from '../assets/Pet, Animal _ dog, training, love, care, woman, home, girl, happy, leisureTEST.svg';
 
 const App = () => {
-  return <div>Hello Worlda</div>;
+  return (
+    <>
+      <div className={style.test}>Hello Worlda</div>
+      <Test />
+    </>
+  );
 };
 
 export default App;
