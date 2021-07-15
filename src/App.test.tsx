889f2dd81665416f97bbc00a16b2cf19853b491d
Luke_Z <zurawskiu@gmail.com>
2021-07-15 16:20:36 +0200

    Jest work fine. Added all neccesary packages

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,7 @@
  */
 /* eslint-env jest */
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 import App from './components/App';
